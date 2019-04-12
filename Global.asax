e7577dbebc42462346c67b004d9dbb1ba9d3917c
avinash-1997 <49508678+avinash-1997@users.noreply.github.com>
2019-04-12 11:02:36 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PaytmTest1.MvcApplication" Language="C#" %>
---
+++ Key.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaytmTest1
+{
+    public class Key
+    {
+        public static string merchantKey = "VRmpULd&OpdLtrzU";
+        public static string merchantid = "szZaIE67886296228797";
+    }
+}
